8257c5ecddf7531a31d636f3d8239e1d434177eb
SEC-MASUM <masum.jess42@gmail.com>
2022-10-07 21:18:17 +0600

    style home component and add logic

--- src/app/crud/home/home.component.ts
+++ src/app/crud/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../crud.service';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,21 @@ import { CrudService } from '../crud.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  posts: any;
+  posts: Post[] = [];
+
   constructor(public crudService: CrudService) {}
 
   ngOnInit(): void {
-    this.crudService.getAll().subscribe((res) => {
+    this.crudService.getAll().subscribe((res: Post[]) => {
       console.log(res);
       this.posts = res;
+    });
+  }
+
+  deletePost(id: number) {
+    this.crudService.delete(id).subscribe((res) => {
+      this.posts = this.posts.filter((post) => post.id !== id);
+      console.log("Post deleted successfully");
     });
   }
 }
